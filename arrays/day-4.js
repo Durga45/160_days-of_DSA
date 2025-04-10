f631295ad4dfac2dc7d02398dc135b64af424499
Durga45 <dspandey051@gmail.com>
2025-04-10 15:47:02 +0530

    day4

---
+++ arrays/day-4.js
@@ -0,0 +1,15 @@
+function reverse(arr,start,end){
+  while(start<end){
+    [arr[start],arr[end]]=[arr[end],arr[start]]
+    start++
+    end--
+  }
+  return arr
+}
+function rotateArray(arr,k){
+  const n=arr.length
+  k=k%n
+  reverse(arr,0,k-1)
+  reverse(arr,k.n-1)
+  reverse(arr,0,n-1)
+}
